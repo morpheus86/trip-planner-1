beeb75ecacbb09f103e09342dca4139829bfd1e5
Andrew Miller <admiller9@gmail.com>
2018-06-19 16:35:03 -0400

    first commmit

---
+++ src/marker.js
@@ -0,0 +1,19 @@
+const mapbox = require("mapbox-gl");
+
+const iconURLs = {
+    hotel: "http://i.imgur.com/D9574Cu.png",
+    restaurant: "http://i.imgur.com/cqR6pUI.png",
+    activity: "http://i.imgur.com/WbMOfMl.png"
+};
+
+const buildMarker = (type, coords) => {
+  const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+  markerDomEl.style.width = "32px";
+  markerDomEl.style.height = "39px";
+  markerDomEl.style.backgroundImage = `url(${iconURLs[type]})`;
+
+  const marker = new mapbox.Marker(markerDomEl).setLngLat(coords);
+  return marker
+};
+
+module.exports = buildMarker;
